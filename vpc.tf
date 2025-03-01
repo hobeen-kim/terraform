64c02111011e93001e2167da250cef8d25953964
hobeen-kim <sksjsksh32@naver.com>
2025-03-01 21:43:21 +0900

    public rt 에 route {} 추가

--- vpc.tf
+++ vpc.tf
@@ -94,6 +94,11 @@ resource "aws_nat_gateway" "nat_gateway_2" {
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   tags = {
     Name = "main"
   }
@@ -136,13 +141,13 @@ resource "aws_route_table_association" "route_table_association_private_2" {
 }
 
 resource "aws_route" "private_nat_1" {
-  route_table_id              = aws_route_table.route_table_private_1.id
-  destination_cidr_block      = "0.0.0.0/0"
-  nat_gateway_id              = aws_nat_gateway.nat_gateway_1.id
+  route_table_id         = aws_route_table.route_table_private_1.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_1.id
 }
 
 resource "aws_route" "private_nat_2" {
-  route_table_id              = aws_route_table.route_table_private_2.id
-  destination_cidr_block      = "0.0.0.0/0"
-  nat_gateway_id              = aws_nat_gateway.nat_gateway_2.id
+  route_table_id         = aws_route_table.route_table_private_2.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gateway_2.id
 }
